b0dfcc3e3b7e9f6d9181b248c86469f634600b94
kin-au <kinnamau@gmail.com>
2020-01-18 17:14:59 +0000

    Fix popup window size

---
+++ extension/popup.js
@@ -0,0 +1,12 @@
+const updateContent = event => {
+  event.preventDefault();
+
+  const url = document.getElementById('input-url').value;
+  const glitchedContent = document.getElementById('iframe-content');
+
+  const glitchedUrl = 'https://glitched.news/article?url=' + url;
+  glitchedContent.setAttribute('src', glitchedUrl);
+};
+
+const button = document.getElementById('button-get-info');
+button.addEventListener('click', updateContent);
