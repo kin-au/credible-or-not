4694cd1a88a2c79040d5b45169f557d188320c75
Alexandra O'Mahony <alexandra.omahony1@gmail.com>
2020-01-19 15:36:00 +0000

    Edited popup html and added css

--- extension/content.js
+++ extension/content.js
@@ -1,9 +1,13 @@
+let filledDiv;
 const updateContent = () => {
-  const iframe = document.createElement('iframe');
+  const iframe = document.createElement("iframe");
   document.body.parentNode.insertBefore(iframe, document.body);
 
   const glitchedUrl =
-    'https://glitched.news/article?url=' + window.location.href;
-  iframe.setAttribute('src', glitchedUrl);
+    "https://glitched.news/article?url=" +
+    window.location.href +
+    "#article-sentiment";
+
+  iframe.setAttribute("src", glitchedUrl);
 };
-updateContent();
+updateContent().addEventListener("load", () => console.log(filledDiv));
